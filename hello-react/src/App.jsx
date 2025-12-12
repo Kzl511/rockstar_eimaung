cbec1ffe0432e637acf77b6e97410e6098240f36
Kzl511 <kyawlinn.23@ichat.sp.edu.sg>
2025-12-12 17:47:32 +0800

    3

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,14 +1,85 @@
-import List from "./List.jsx";
-import Item from "./Item.jsx";
+import { useContext, useState } from "react";
+
+import { AppContext } from "./ThemedApp";
+
+import List from "./deprecated/List.jsx";
+import Item from "./components/Item.jsx";
+import Form from "./components/Form.jsx";
 
 export default function App() {
+  const { mode, setMode } = useContext(AppContext);
+
+  const [showForm, setShowForm] = useState(false);
+  const [data, setData] = useState([
+    { id: 1, content: "Hello, World!", name: "Alice" },
+    { id: 2, content: "React is fun.", name: "Bob" },
+    { id: 3, content: "Yay, interesting", name: "Chris" },
+  ]);
+
+  const remove = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+  };
+
   return (
-    <div style={{ maxWidth: 600, margin: "20px auto"}}>
-      <h1>Yaycha</h1>
-      <List>
-        <Item content="Hello, World!" name="Alice"></Item>
-        <Item content="React is fun." name="Bob"></Item>
-      </List>
+    <div
+      style={{
+        minHeight: 1500,
+        background: mode === "dark" ? "black" : "white",
+        color: mode === "dark" ? "white" : "black",
+        paddingTop: 20,
+      }}
+    >
+      <div style={{ maxWidth: 600, margin: "20px auto" }}>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          Foodie
+          <button
+            onClick={() => setShowForm(!showForm)}
+            style={{
+              width: 32,
+              height: 32,
+              borderRadius: 50,
+              border: "0 none",
+              background: showForm ? "#dc3545" : "#0d6efd",
+              color: "white",
+            }}
+          >
+            {showForm ? "x" : "+"}
+          </button>
+          <button
+            onClick={() => {
+              setMode(mode === "dark" ? "light" : "dark");
+            }}
+            style={{
+              marginLeft: 8,
+              padding: "0 24px",
+              height: 32,
+              borderRadius: 32,
+              border: "0 none",
+              background: mode === "dark" ? "#333" : "#ddd",
+              color: mode === "dark" ? "white" : "black",
+            }}
+          >
+            {mode === "dark" ? "Light" : "Dark"}
+          </button>
+        </h1>
+        {showForm && <Form add={add} />}
+        <List>
+          {data.map((item) => (
+            <Item key={item.id} item={item} remove={remove} />
+          ))}
+        </List>
+      </div>
     </div>
   );
 }
--- hello-react/src/Item.jsx
+++
@@ -1,7 +0,0 @@
-export default function Item({ content, name }) {
-    return (
-        <li style={{ padding: 10, borderBottom: "1px solid #ddd" }}>
-            {content} - <b>{name}</b>
-        </li>
-    );
-}
